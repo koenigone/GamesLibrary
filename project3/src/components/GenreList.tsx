085b25e6562e5ab712bd1d6034a60826260c234c
Mohammad Mohammad <mm6dd.mh@gmail.com>
2023-09-14 19:30:07 +0100

    clean up the genres section

--- project3/src/components/GenreList.tsx
+++ project3/src/components/GenreList.tsx
@@ -5,6 +5,7 @@ import {
   HStack,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 
@@ -20,35 +21,38 @@ function GenreList({ onSelectGenre, selectedGenre }: Props) {
   if (isLoading) return <Spinner thickness="4px" size="lg" />;
 
   return (
+    <HStack display='block' marginLeft={5}>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => {
 
         // Shortening the genre's name for a better look
         const ShortenName = genre.name === 'Massively Multiplayer' ? 'Multiplayer' : (genre.name === 'Board Games' ? 'Board' : genre.name);
 
-        
         return (
-          <ListItem key={genre.id} paddingY="7px" paddingX="20px">
-            <HStack spacing={4}>
-              <Image
-                src={genre.image_background}
-                boxSize="35px"
-                borderRadius={10}
-              />
-              <Button
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                onClick={() => onSelectGenre(genre)}
-                marginLeft='23px'
-                fontSize="md"
-                variant="link"
-              >
-                { ShortenName }
-              </Button>
-            </HStack>
-          </ListItem>
-        );
-      })}
-    </List>
+            <ListItem key={genre.id} paddingY="7px">
+              <HStack spacing={4}>
+                <Image
+                  src={genre.image_background}
+                  boxSize="35px"
+                  objectFit='cover'
+                  borderRadius={10}
+                />
+                <Button
+                  fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                  onClick={() => onSelectGenre(genre)}
+                  marginLeft='5px'
+                  fontSize="md"
+                  variant="link"
+                >
+                  { ShortenName }
+                </Button>
+              </HStack>
+            </ListItem>
+          );
+        })}
+      </List>
+    </HStack>
   );
 }
 
